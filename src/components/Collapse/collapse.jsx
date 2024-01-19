49c76711ac6ade641e9f8d7cd6da4dbfa3df07bb
hugo566 <hugo56.lg@gmail.com>
2024-01-19 11:36:08 +0100

    Créez la page À propos

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -12,12 +12,17 @@ export default function Collapse(props) {
     return (
         <div className="collapse-container">
             <div className="collapse-title" onClick={eventToggle}>
-                <h2>{props.title}</h2>
-                <i className={toggle ? 'fa-solid fa-chevron-up arrow-up' : 'fa-solid fa-chevron-up arrow-down'}></i>
+                <div className="title">
+                    <h2>{props.title}</h2>
+                    <i className={toggle ? 'fa-solid fa-chevron-up arrow-up' : 'fa-solid fa-chevron-up arrow-down'}></i>
+                </div>
+
+                <div className={toggle ? 'collapse-txt' : 'collapse-txt-hidden'}>
+                    <p>{props.text}</p>
+                </div>
+
             </div>
-            <div className={toggle ? 'collapse-txt' : 'collapse-txt-hidden'}>
-                <p>{props.text}</p>
-            </div>
+
         </div >
     )
 }
