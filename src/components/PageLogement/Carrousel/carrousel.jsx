2a0f623585e8f7cac692e3d500e8e812dfad674a
hugo566 <hugo56.lg@gmail.com>
2024-01-22 16:09:16 +0100

    Création du carrousel

--- src/components/PageLogement/Carrousel/carrousel.jsx
+++ src/components/PageLogement/Carrousel/carrousel.jsx
@@ -1,21 +1,40 @@
 import './carrousel.scss'
 import logements from '../../../../public/logements.json'
 import { useParams } from "react-router-dom";
+import arrowLeft from '../../../assets/images/arrow_Left.png'
+import arrowRight from '../../../assets/images/arrow_Right.png'
+import { useState } from 'react'
 
 export default function Carrousel() {
     const { id } = useParams();
 
     const ficheLogement = logements.find((logement) => logement.id === id);
-    const imageLogement = ficheLogement.pictures.map((imageLogement) => {
-        return (
-            <img className="imageCarrousel" src={imageLogement} />
-        )
-    })
+
+    const [index, setIndex] = useState(0)
+
+    const nextImg = () => {
+        setIndex(index + 1)
+        if (index === ficheLogement.pictures.length - 1)
+            setIndex(0)
+    }
+
+    const lastImg = () => {
+        setIndex(index - 1)
+        if (index === 0)
+            setIndex(ficheLogement.pictures.length - 1)
+    }
 
     return (
 
         <div className="containerCarrousel">
-            {imageLogement}
+            <img className="imageCarrousel" src={ficheLogement.pictures[index]} />
+            <div className="arrow">
+                <img src={arrowLeft} alt="arrowLeft" onClick={lastImg} />
+                <img src={arrowRight} alt="arrowRight" onClick={nextImg} />
+            </div>
+            <div className="indice">
+                <p><span>{index + 1}</span> / <span>{ficheLogement.pictures.length}</span></p>
+            </div>
         </div>
     )
 }
